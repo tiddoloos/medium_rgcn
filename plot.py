8158abf5a8f8a3bc59d2f12603aa2ae615035600
tiddoloos <tiddoloos@hotmail.com>
2022-11-15 17:40:31 +0100

    plot settings

--- plot.py
+++ plot.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_results(epochs: int, y: list, title: str, y_label: str) -> None:
 
     x = list(range(epochs))
 
-    plt.margins(x=0)
     plt.xlabel('Epochs')
     plt.ylabel(y_label)
+    plt.yticks(np.arange(0, 1.1, step=0.1))
+    plt.ylim([0, 1])
+    plt.margins(x=0)
+    plt.margins(y=0)
     plt.title(title)
     plt.plot(x, y, label=y_label)
+    plt.savefig(f'./figures/{y_label}.png')
     plt.show()
